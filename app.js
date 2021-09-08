85e07e8bc01cf2878c3073fc7844f896f647203e
Cristalsys <cristals22332233n@gmail.com>
2021-09-08 20:17:17 +0300

    four commit

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === 'production') {
 
 }
 
-const PORT = config.get('port') || 5000
+const PORT = process.env.PORT || 5000
 
 // middleware
 app.use('/uploads', express.static('uploads'))
